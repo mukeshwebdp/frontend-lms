23c5c8ab2c7feb6632bb0ae1250dbcd90648dce1
mukeshwebdp <mukesh.webdp@gmail.com>
2025-03-26 14:11:52 +0530

    add settings like redux, axios, slices

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
+import store from './Redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store} >
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
